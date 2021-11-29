025c616a72446c47ca98961b20ba486b824d2cca
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-11-29 10:46:08 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template><header></header></template>
+
+<script>
+export default {
+	name: "Header",
+	data() {
+		return {};
+	},
+	props: {},
+	methods: {},
+	computed: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
